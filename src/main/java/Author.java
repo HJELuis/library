bbebd3d27575d6450588c001fdcf347a71c9129a
HJELuis <lenrique.lejh@gmail.com>
2024-09-15 13:45:06 -0600

    adding the Author class

---
+++ src/main/java/Author.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+public class Author {
+    private String name;
+    private String lastName;
+    private String biography;
+    private Set<Book> publishedBooks;
+
+    public Author(String name, String lastName, String biography, Set<Book> publishedBooks) {
+        this.name = name;
+        this.lastName = lastName;
+        this.biography = biography;
+        this.publishedBooks = publishedBooks;
+    }
+
+}
